dd7e281de5ababc90082b179360f68eadca288c7
deepadev1610 <38211643+deepadev1610@users.noreply.github.com>
2019-04-10 21:01:39 +0530

    Create about.blade.php

---
+++ resources/views/about.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.master')
+@section('content')
+<div class="jumbotron">
+  <h1 class="display-4">Hello, Reader!</h1>
+  <p class="lead">Bibliophile is a platform for readers to kindle their love for reading.</p>
+  <hr class="my-4">
+  <p>Books act as a medium that bring to you the world of learning and knowledge. From timeless classics to fictions, books for children and teenagers, school books and books for competitive exams, they improve our vocabulary and increase our knowledge. The nursery rhymes books, comics and graphic novels, and illustrative story books help the young minds grow a power of imagination, while the characters give them knowledge about the world. Buying books online will make sure that every member of your family has a book that suits their reading needs.
+</p>
+  <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+</div>
+@endsection
